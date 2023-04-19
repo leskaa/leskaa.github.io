021fdb4ec48ac1aed3451f041e0c0b71410ee758
Alex Leska <deskmicealex@gmail.com>
2023-04-19 11:28:20 -0700

    Update astro to v2 and change site base from beta to root

--- astro.config.ts
+++ astro.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
@@ -6,6 +6,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
-  site: 'https://leskaa.github.io/',
-  base: '/beta'
+  site: "https://leskaa.github.io/",
 });
